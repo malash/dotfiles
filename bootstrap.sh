b5a03612b261f23ba4547d92fba128aeca36f54a
Malash <i@malash.me>
2015-08-19 00:02:57 +0800

    remove no used files

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "fonts/" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "fonts/" \
+        --exclude ".osx" --exclude "brew.sh" --exclude "init" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
