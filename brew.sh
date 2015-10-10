20b1e0e424bccea6a7a37d776369bf0f919ba358
Malash <i@malash.me>
2015-10-10 17:45:37 +0800

    fix git osxkeychain

--- brew.sh
+++ brew.sh
@@ -93,12 +93,8 @@ brew install ack
 brew install dark-mode
 #brew install exiv2
 brew install git
+brew link git
 brew install git-lfs
-# Config git credential-osxkeychain
-curl -s -O https://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
-chmod u+x git-credential-osxkeychain
-mv git-credential-osxkeychain "$(dirname $(which git))/git-credential-osxkeychain"
-git config --global credential.helper osxkeychain
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
